53d7addfb4569786ca19950d56cc600c881d2ef7
xFlyyer <pajamacruzer@gmail.com>
2018-12-06 09:54:32 -0600

    iouyhj

--- src/aSSBLASTER50000/ASSBlaster50000.java
+++ src/aSSBLASTER50000/ASSBlaster50000.java
@@ -3,7 +3,7 @@ package aSSBLASTER50000;
 public class ASSBlaster50000 {
 
 	public static void main(String[] args) {
-		//ASSGui blaster = new ASSGui();
+		ASSGui blaster = new ASSGui();
 		ASSwordCheck ass=new ASSwordCheck("user", "password");
 		System.out.println("|||||||||||||||||||||||");
 		System.out.println(ass.idCheck("Strink", "owo"));
